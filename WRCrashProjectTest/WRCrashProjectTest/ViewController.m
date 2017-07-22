53c782122ba24d9c3ad9106cf24919ff13193dd2
wangrui460 <wangruidev@gmail.com>
2017-07-22 22:30:24 +0800

    init

---
+++ WRCrashProjectTest/WRCrashProjectTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WRCrashProjectTest
+//
+//  Created by wangrui on 2017/7/22.
+//  Copyright © 2017年 wangrui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
